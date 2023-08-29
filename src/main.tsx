7b76fe3e876e83cab2eec3a65f245afd66a68514
gugaSouza-dev <gustavo_souza.dev@outlook.com>
2023-08-29 20:21:53 -0300

    rota da pagina de perfil parceiro

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,20 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+
+//pages
+import PerfilParceiro from "./pages/Perfil";
+
+//rotas
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    
+    <BrowserRouter>
+	<Routes>
+		<Route path='/perfil/parceiro' element={<PerfilParceiro/>}/>
+	</Routes>
+	</BrowserRouter>
   </React.StrictMode>,
 )
