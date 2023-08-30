ec8abcfb6fa1c3f610da96a6970af510eac262fd
Lidiane Lili <gustavo_souza.dev@outlook.com>
2023-08-30 01:32:26 -0300

    pagina de Privacidade com css padronizado

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,8 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 
 //pages
-import PerfilParceiro from "./pages/Perfil";
+// import PerfilParceiro from "./pages/Perfil";
+import Privacidade from "./pages/Privacidade";
 
 //rotas
 import { BrowserRouter, Routes, Route } from "react-router-dom";
@@ -13,7 +14,8 @@ ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <BrowserRouter>
 	<Routes>
-		<Route path='/perfil/parceiro' element={<PerfilParceiro/>}/>
+		{/* <Route path='/perfil/parceiro' element={<PerfilParceiro/>}/> */}
+		<Route path='/privacidade' element={<Privacidade/>}/>
 	</Routes>
 	</BrowserRouter>
   </React.StrictMode>,
---
+++ src/pages/Privacidade/index.tsx
@@ -0,0 +1,77 @@
+import "./style.css";
+
+function Privacidade() {
+    return (
+        <>
+            <main className="privacidade">
+                <section className="conteudo posicionamento">
+                    <h1>Políticas de Privacidade</h1>
+                    <div className="topico">
+                        <h2>Políticas de Privacidade</h2>
+                        <p>
+                            Podemos coletar informações pessoais quando você se registra em nosso
+                            site, faz uma compra, preenche um formulário ou participa de uma
+                            pesquisa. As informações pessoais que coletamos podem incluir seu nome,
+                            endereço de e-mail, endereço residencial, número de telefone,
+                            informações de pagamento e outras informações que você escolher
+                            compartilhar conosco. Uso de Informações Pessoais Podemos usar as
+                            informações pessoais que coletamos para fornecer nossos serviços,
+                            melhorar nosso site, enviar e-mails promocionais, processar pagamentos,
+                            responder a solicitações de atendimento ao cliente e fornecer suporte
+                            técnico.
+                        </p>
+                    </div>
+                    <div className="topico">
+                        <h2>Coleta de Informações Pessoais</h2>
+                        <p>
+                            Podemos coletar informações pessoais quando você se registra em nosso
+                            site, faz uma compra, preenche um formulário ou participa de uma
+                            pesquisa. As informações pessoais que coletamos podem incluir seu nome,
+                            endereço de e-mail, endereço residencial, número de telefone,
+                            informações de pagamento e outras informações que você escolher
+                            compartilhar conosco. Uso de Informações Pessoais Podemos usar as
+                            informações pessoais que coletamos para fornecer nossos serviços,
+                            melhorar nosso site, enviar e-mails promocionais, processar pagamentos,
+                            responder a solicitações de atendimento ao cliente e fornecer suporte
+                            técnico.
+                        </p>
+                    </div>
+                    <div className="topico">
+                        <h2>Compartilhamento de Informações Pessoais</h2>
+                        <p>
+                            Não vendemos, trocamos ou transferimos suas informações pessoais para
+                            terceiros, exceto para nossos provedores de serviços que nos auxiliam na
+                            operação do site e no fornecimento de serviços. Esses provedores de
+                            serviços são obrigados a manter a confidencialidade das informações que
+                            compartilhamos com eles e não estão autorizados a usar essas informações
+                            para outros fins além de fornecer serviços em nosso nome.
+                        </p>
+                    </div>
+                    <div className="topico">
+                        <h2>Proteção de Informações Pessoais</h2>
+                        <p>
+                            Implementamos medidas de segurança físicas, eletrônicas e
+                            administrativas para proteger as informações pessoais que coletamos
+                            contra acesso não autorizado ou uso indevido. No entanto, nenhum método
+                            de transmissão pela Internet ou armazenamento eletrônico é 100% seguro,
+                            e não podemos garantir a segurança das informações que você nos envia.
+                        </p>
+                    </div>
+                    <div className="topico">
+                        <h2>Alterações na Política de Privacidade</h2>
+                        <p>
+                            Podemos atualizar esta Política de Privacidade de tempos em tempos, e
+                            essas alterações serão publicadas em nosso site. É sua responsabilidade
+                            verificar regularmente esta página para ficar atualizado sobre quaisquer
+                            alterações na Política de Privacidade. Contato Se você tiver alguma
+                            dúvida ou preocupação sobre nossa Política de Privacidade, entre em
+                            contato conosco por meio do e-mail ou telefone fornecido em nosso site.
+                        </p>
+                    </div>
+                </section>
+            </main>
+        </>
+    )
+}
+
+export default Privacidade;
