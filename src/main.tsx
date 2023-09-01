504fcc28cada3da1087904739850a47210efa130
monique <moniquehelen600@gmail.com>
2023-08-31 21:48:05 -0300

    cadastro etapa 2

--- src/main.tsx
+++ src/main.tsx
@@ -3,12 +3,14 @@ import ReactDOM from 'react-dom/client'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import './index.css'
 import CadastroUsuario from './pages/Cadastro/Usuario'
+import UsuarioEtapa2 from './pages/UsuarioEtapa2'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<CadastroUsuario/>}/>
+        <Route path="/usuario/etapa2"element={<UsuarioEtapa2/>}/>
       </Routes>
     </BrowserRouter>
   </React.StrictMode>,
---
+++ src/pages/UsuarioEtapa2/index.tsx
@@ -0,0 +1,79 @@
+import "./style.css"
+
+import { Link } from "react-router-dom";
+
+//imagens
+import ilutracaoCadastro from "../../assets/images/ilustra_cadastro.svg";
+import seta from "../../assets/images/seta.png";
+
+function UsuarioEtapa2() {
+    return(
+        <>
+         <main>
+            <div className="conteudo">
+                <div className="alinhamento_imagens">
+                <div className="image_voltar">
+                    <Link to={"#"} className="botao_voltar">
+                    {" "}
+                    <img src={seta} alt="" />
+                    </Link>
+                    <div>
+                    <Link to={"#"} className="botao_voltar">
+                        {" "}
+                        Voltar
+                    </Link>
+                    </div>
+                </div>
+                {/* <img class="logo" src="../geral/img_login/logo2.png" alt=""> */}
+                <img className="ilustracao" src={ilutracaoCadastro} alt="" />
+                </div>
+                <div className="pagina">
+                <div className="image_voltar_responsivo">
+                    <Link to={"#"} className="botao_voltar">
+                    {" "}
+                    </Link>
+                    <div>
+                    <Link to={"#"} className="botao_voltar">
+                        {" "}
+                        Voltar
+                    </Link>
+                    </div>
+                </div>
+                <div className="bem_vindo">
+                    <span>Bem vindo!</span>
+                    <h1>Crie sua conta!</h1>
+                </div>
+                <div className="dados_usuario">
+                    <form action="">
+                    <div>
+                        <label htmlFor="nome">Senha</label>
+                        <input type="nome" id="nome" defaultValue="******" />
+                    </div>
+                    <div>
+                        <label htmlFor="email">Confirmar senha</label>
+                        <input type="email" id="email" defaultValue="******" />
+                    </div>
+                    <div className="bc">
+                        <Link
+                        className="button_cont"
+                       to={"#"}
+                        >
+                        Finalizar
+                        </Link>
+                    </div>
+                    </form>
+                </div>
+                <div className="conta_cadastro">
+                    {/* <span href="">Já tem uma conta?</span> */}
+                    <Link to={"#"}>Já tem uma conta? <span> Se logar</span></Link>
+                </div>
+                </div>
+            </div>
+        </main>
+
+      </>
+    )
+}
+
+export default UsuarioEtapa2;
+    
