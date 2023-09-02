82fb924cfac6d6d6d59bf80a8390232b9af7911c
Lidiane Lili <gustavo_souza.dev@outlook.com>
2023-09-01 22:38:21 -0300

    rotas atualizadas e etapa de cadastro um e dois dentro do mesmo diretorio

--- src/main.tsx
+++ src/main.tsx
@@ -3,13 +3,13 @@ import ReactDOM from 'react-dom/client'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import './index.css'
 import CadastroUsuario from './pages/Cadastro/Usuario'
-import UsuarioEtapa2 from './pages/UsuarioEtapa2'
+import UsuarioEtapa2 from './pages/Cadastro/UsuarioEtapa2'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<CadastroUsuario/>}/>
+        <Route path="/usuario/etapa1" element={<CadastroUsuario/>}/>
         <Route path="/usuario/etapa2"element={<UsuarioEtapa2/>}/>
       </Routes>
     </BrowserRouter>
