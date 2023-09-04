38ab0a3da9a0b84952483d0097e373fc60131ba2
vinicius <viniolimpio989@gmail.com>
2023-09-04 20:05:04 -0300

    concluido a tela home

--- src/main.tsx
+++ src/main.tsx
@@ -2,8 +2,23 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './index.css'
 
+//componentes
+import Home from "./pages/Home";
+
+//estilização global
+import "./index.css";
+
+//rotas
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+
+
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    
+   <BrowserRouter>
+      <Routes>{/*Indica uma lista de rotas*/}
+      <Route path='/' element={<Home />} /> {/*Indica o caminho do componente e o nome da rota dele*/}
+    </Routes>
+   </BrowserRouter>
   </React.StrictMode>,
 )
