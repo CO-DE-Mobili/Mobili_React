6857ea7f1bc235c948d880f38ed15b79b5bcfd9b
Gisele <giselesenai2022@gmail.com.br>
2023-09-04 20:14:42 -0300

    finalização página login

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,15 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import './index.css'
+import Login from './pages/Login'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    
+    <BrowserRouter>
+      <Routes>
+        <Route path='/login' element={<Login />} />
+      </Routes>
+    </BrowserRouter>
   </React.StrictMode>,
 )
