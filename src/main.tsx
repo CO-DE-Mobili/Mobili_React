9780926b8abe8db4845f1c593f30ffc3bbcbc25c
lukstryke <lukstryke.imb@gmail.com>
2023-09-04 21:43:04 -0300

    correcoes header e footer

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,16 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './index.css'
+import Footer from './components/Footer'
+import Header from './components/Header'
+import { BrowserRouter } from 'react-router-dom'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
+    <BrowserRouter> 
+      <Header/>
+      <Footer/>
+    </BrowserRouter>
     
   </React.StrictMode>,
 )
