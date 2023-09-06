eeade0983676b0c127a0e82ba3aafc10f2330de6
monique <moniquehelen600@gmail.com>
2023-09-05 21:37:07 -0300

    conclusao da tela proganda parceiro

--- src/main.tsx
+++ src/main.tsx
@@ -13,14 +13,15 @@ import Editar_perfil_parceiro from './pages/editar_perfilParceiro';
 import Gerenciar_perfil_parceiro from './pages/gerenciar_perfilParceiro';
 
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Propaganda_PerfilParceiro from './pages/Propaganda_PerfilParceiro';
 
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <BrowserRouter>
     <Routes>
-
-    <Route path='/' element={<Gerenciar_perfil_parceiro />} /> 
+    <Route path='/' element={<Gerenciar_perfil_parceiro />} />
+    <Route path='propaganda/perfilparceiro' element={<Propaganda_PerfilParceiro/>}/>
     </Routes>
     </BrowserRouter>
   </React.StrictMode>
