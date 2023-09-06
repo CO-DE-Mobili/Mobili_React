1b9880c04cc8ab44946d9d22d2e7c62769757d8a
gugaSouza-dev <gustavo_souza.dev@outlook.com>
2023-09-06 20:12:52 -0300

    organiza

--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,6 @@ import Login from './pages/Login'
 
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import Header from './components/Header'
 
 //pages
 
@@ -21,7 +20,6 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <BrowserRouter>
-      <Header />
       <Routes>{/*Indica uma lista de rotas*/}
         <Route path='/' element={<Home />} /> {/*Indica o caminho do componente e o nome da rota dele*/}
         <Route path='/login' element={<Login />} />
