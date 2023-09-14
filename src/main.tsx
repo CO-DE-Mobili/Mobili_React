6f25b125f3ca15ffef6677b218ec82a5fca48e28
gugaSouza-dev <gustavo_souza.dev@outlook.com>
2023-09-14 20:26:31 -0300

    cadastro empresa (estrutura)

--- src/main.tsx
+++ src/main.tsx
@@ -5,6 +5,7 @@ import ReactDOM from 'react-dom/client'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 
 //Pages
+import CadastroEmpresa from './pages/CadastroEmpresa'
 import CadastroUsuario from './pages/Cadastro/Usuario'
 import UsuarioEtapa2 from './pages/Cadastro/UsuarioEtapa2'
 import RedefinirSenha from "./pages/RedefinirSenha";
@@ -26,6 +27,7 @@ ReactDOM.createRoot(document.getElementById('root')!).render(
 
 				<Route path='/quem_somos' element={<QuemSomos/>}/>
 				<Route path='/privacidade' element={<Privacidade/>}/>
+    		    <Route path='/empresa/cadastro' element={<CadastroEmpresa/>}/>
 
 				<Route path='/' element={<Home />} /> {/*Indica o caminho do componente e o nome da rota dele*/}
 				<Route path='/login' element={<Login />} />
---
+++ src/pages/CadastroEmpresa/index.tsx
@@ -0,0 +1,147 @@
+import "./style.css"
+import { Link } from "react-router-dom";
+import Select from 'react-select'
+
+const categorias = [
+  { value: 'restaurante', label: 'Restaurante' },
+  { value: 'hamburgueria', label: 'Hamburgueria' },
+  { value: 'posto de Gasolina', label: 'Posto de Gasolina' },
+  { value: 'pizzaria', label: 'Pizzaria' },
+  { value: 'floricultura', label: 'Floricultura' }
+]
+
+const paises = [
+	{value: 'brasil', label: 'Brasil'}
+]
+
+
+function CadastroEmpresa() {
+	return (
+		<>
+			<main className="main-cadastro_empresa">
+				<section className="container">
+					<h1>Cadastro de Empresa</h1>
+					<div className="formulario">
+						<form action="POST">
+							<div className="nome">
+								<div>
+									<label htmlFor="nome_loja">Nome</label>
+									<input
+										type="text"
+										placeholder="Informe o nome da sua empresa"
+										id="nome_empresa"
+										/>
+								</div>
+								<div>
+									<label htmlFor="nome_fantasia">Nome Fantasia</label>
+									<input
+										type="text"
+										placeholder="Informe o nome fantasia da sua empresa"
+										id="nome_fantasia"
+										/>
+								</div>
+								<div>
+									<label htmlFor="razao_social">Razão Social</label>
+									<input
+										type="text"
+										placeholder="Informe a razão social da sua empresa"
+										id="razao_social"
+										/>
+								</div>
+								<div>
+									<label htmlFor="telefone">Telefone de contato</label>
+									<input
+										type="text"
+										placeholder="(00) 1234-5678"
+										id="razao_social"
+										/>
+								</div>
+							</div>
+							<hr></hr>
+							<div>
+								<label htmlFor="cpf">CPF/CNPJ</label>
+								<input
+									type="text"
+									id="cpf"
+									placeholder="Seu E-mail aqui"
+									/>
+							</div>
+							<div>
+								<label htmlFor="select_categorias">Categoria de serviço</label>
+								<Select options={categorias} id="select_categorias" className="select_categorias"/>
+							</div>
+							<div>
+								<label htmlFor="cnae">CNAE</label>
+								<input
+									type="text"
+									placeholder="Informe o CNAE da sua empresa"
+									id="cnae"
+									/>
+							</div>
+							<hr></hr>
+							<div>
+								<label htmlFor="select_categorias">País/Região</label>
+								<Select options={paises} id="select_categorias"/>
+							</div>
+							<div>
+								<label htmlFor="endereco">Endereço</label>
+								<input
+									type="text"
+									placeholder="Nome da rua"
+									id="endereco"
+								/>
+							</div>
+							<div>
+								<label htmlFor="numero">Numero do estabelecimento</label>
+								<input
+									type="text"
+									placeholder="00"
+									id="numero"
+								/>
+							</div>
+							<div>
+								<label htmlFor="complemento">Complemento (opcional)</label>
+								<input
+									type="text"
+									placeholder="Apartamento, sala, conjunto, edificio, etc."
+									id="complemento"
+								/>
+							</div>
+							<div>
+								<label htmlFor="bairro">Bairro</label>
+								<input
+									type="text"
+									placeholder="Nome do bairro"
+									id="bairro"
+								/>
+							</div>
+							<div>
+								<label htmlFor="cidade">Cidade</label>
+								<input
+									type="text"
+									placeholder=""
+									id="cidade"
+								/>
+							</div>
+							<div>
+								<label htmlFor="estado">Estado</label>
+								<input
+									type="text"
+									placeholder=""
+									id="estado"
+								/>
+							</div>
+							<div className="botao_submit">
+								<a className="confirmar">
+									Confirmar
+								</a>
+							</div>
+						</form>
+					</div>
+				</section>
+			</main>
+		</>
+	)
+}
+
+export default CadastroEmpresa;
