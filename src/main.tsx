6ac4e003be1fe5d86dcb67f2dece775724a0938b
gugaSouza-dev <gustavo_souza.dev@outlook.com>
2023-09-15 19:40:09 -0300

    organizando as rotas dentro da main

--- src/main.tsx
+++ src/main.tsx
@@ -5,7 +5,6 @@ import ReactDOM from 'react-dom/client'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 
 //Pages
-import CadastroEmpresa from './pages/CadastroEmpresa'
 import CadastroUsuario from './pages/Cadastro/Usuario'
 import UsuarioEtapa2 from './pages/Cadastro/UsuarioEtapa2'
 import RedefinirSenha from "./pages/RedefinirSenha";
@@ -19,6 +18,7 @@ import Perfil_parceiro from './pages/Perfil_Parceiro';
 import Editar_perfil_parceiro from './pages/PerfilParceiro_Editar';
 import Propaganda_PerfilParceiro from './pages/Propaganda_PerfilParceiro';
 import Gerenciar_perfil_parceiro from './pages/PerfilParceiro_Gerenciar'
+import CadastroEmpresa from './pages/CadastroEmpresa'
 
 //componentes
 
@@ -26,22 +26,27 @@ ReactDOM.createRoot(document.getElementById('root')!).render(
 	<React.StrictMode>
 		<BrowserRouter>
 			<Routes> {/*Indica uma lista de rotas*/}
-				{/* Cadastro */}
+				<Route path='/' element={<Home />} /> {/*Indica o caminho do componente e o nome da rota dele*/}
+
+				{/* Cadastro Usuario */}
 				<Route path="/cadastro/usuario" element={<CadastroUsuario/>}/>
 				<Route path="/cadastro/usuario/etapa2"element={<UsuarioEtapa2/>}/>
 				<Route path="/redefinir/senha"element={<RedefinirSenha/>}/>
 				<Route path='/suporte'element={<Suporte/>}/>
-
-				<Route path='/quem_somos' element={<QuemSomos/>}/>
-				<Route path='/privacidade' element={<Privacidade/>}/>
-    		    <Route path='/empresa/cadastro' element={<CadastroEmpresa/>}/>
-
-				<Route path='/' element={<Home />} /> {/*Indica o caminho do componente e o nome da rota dele*/}
 				<Route path='/login' element={<Login />} />
 
-        {/* Perfil */}
-        <Route path='/perfil_parceiro/gerenciamento' element={<Gerenciar_perfil_parceiro />} />
-        <Route path='/perfil_parceiro/propaganda' element={<Propaganda_PerfilParceiro/>}/>
+				{/* Cadastro Empresa */}
+				<Route path='/cadastro/empresa' element={<CadastroEmpresa/>}/>
+
+				{/* Gerais */}
+				<Route path='/quem_somos' element={<QuemSomos/>}/>
+				<Route path='/privacidade' element={<Privacidade/>}/>
+
+				{/* Perfil */}
+				<Route path='/perfil_parceiro/gerenciamento' element={<Gerenciar_perfil_parceiro />} />
+				{/* <Route path='/perfil_parceiro/propaganda' element={<Propaganda_PerfilParceiro/>}/>
+				<Route path='/perfil_parceiro/editar' element={<Editar_perfil_parceiro/>}/>
+				<Route path='/perfil_parceiro/' element={<Perfil_parceiro/>}/> */}
 			</Routes>
 		</BrowserRouter>
 	</React.StrictMode>,
